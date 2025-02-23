9e8e4f0be5933d87a933225f91e0e54334648883
vraj1763@gmail.com <vraj1763@gmail.com>
2025-02-23 22:46:39 +0530

    minor fix

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        // target: "http://localhost:3000", // Replace with your backend URL
-        target: "https://faculty-allocation-system-server.onrender.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 });
